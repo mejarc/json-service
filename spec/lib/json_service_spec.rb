da177a806582f815d5206684d8050178c5f6467f
Melanie Archer <job@twobanjos.com>
2014-09-26 11:47:00 -0700

    More tests for additional methods

--- spec/lib/json_service_spec.rb
+++ spec/lib/json_service_spec.rb
@@ -2,7 +2,7 @@ require_relative '../spec_helper.rb'
 require_relative '../../lib/json_service'
 
 describe Service do
-  before :each do
+  before :all do
     @txt = "Cupcake ipsum dolor sit amet chocolate cake.  Chocolate bar sweet roll gingerbread 
            brownie marshmallow cheesecake cookie."
     @service = Service.new(@txt)
@@ -18,10 +18,26 @@ describe Service do
     end
   end
 
+  describe "#treat_input" do
+    it "removes extra whitespace from the string" do
+      output = @service.treat_input
+      expect(output).not_to eq(@txt)
+    end
+
+  end  
+
   describe "#accept_input" do
-    it "accept text input, and converts it to JSON" do
-      expect(@service.accept_input).not_to eq(@txt) 
+    before :all do
+      @hsh = @service.accept_input
+    end
+
+    it "accept text input, and converts it to a hash" do
+      expect(@hsh).to have_key("output")
+      expect(@hsh).to have_key("diff")
+    end
+    it "lists the corrections made" do
+      expect(@hsh["diff"]).not_to be_empty 
     end
   end
-  
+
 end
